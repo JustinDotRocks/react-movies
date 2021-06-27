40fa007c489f6a8fb5c8b90347aec0c499b95887
Justin <justins192107@gmail.com>
2021-06-27 15:49:18 -0230

    removed build from gitignore

---
+++ build/precache-manifest.3cc904b1b871a2be4f057d096d305d18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a76d0791f1557045c6291f3cee755b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e0168e5bd9be41181e",
+    "url": "/static/css/main.c7b8395a.chunk.css"
+  },
+  {
+    "revision": "b5fce31e1b0d02028d7f",
+    "url": "/static/js/2.f5c728b8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f5c728b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e0168e5bd9be41181e",
+    "url": "/static/js/main.ce7e0c1c.chunk.js"
+  },
+  {
+    "revision": "ed7a4cf7a6172c63671d",
+    "url": "/static/js/runtime-main.085ffa48.js"
+  }
+]);
